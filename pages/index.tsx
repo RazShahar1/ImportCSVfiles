b85da16dc51c61c4ce3e60a1df89b6651156e537
Raz Shahar <razshahar1@gmail.com>
2023-01-05 15:19:42 +0200

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,12 @@ export default function Home() {
   return (
  <div>
   <h1>Hello User pleace upload file</h1>
+
+  <div className="importdatabtn">
     <UseCSV importerKey="cb6581dc-a874-4498-b1a7-2cd66907ceb8" >
     import data 
     </UseCSV>
+    </div>
 
     <div>
       {/* <Handler/> */}
